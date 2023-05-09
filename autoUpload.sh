31f4cf3f1716d30666125ce5eeb65436a77380e5
root <root@metanet.pri>
2023-05-09 16:29:01 +0900

    2023-05-09 16:29:01 content edit

--- autoUpload.sh
+++ autoUpload.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-read -p "콘솔 입력 _ 커밋 메세지를 입력해주세요: " msg
+if [ $# -eq 1 ]; then
+  msg=$1
+else
+  msg=$(date +"%Y-%m-%d %T")" content edit"
+fi
+
+echo $msg
+
 git add . && git commit -m " $msg " && git push origin master
