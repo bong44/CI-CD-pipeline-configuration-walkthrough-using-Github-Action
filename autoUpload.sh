477d6b0b6a78d9b24e5b3c51a741131e6fd554a9
root <root@metanet.pri>
2023-05-09 09:29:04 +0900

    0509아침수정

--- autoUpload.sh
+++ autoUpload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 read -p "콘솔 입력 _ 커밋 메세지를 입력해주세요: " msg
-git add . && git commit -m " $msg " && git push origin master && bash ./autoUploadLib.sh
+git add . && git commit -m " $msg " && git push origin master
