aba564b145023f7b5ca4d524b20a6d7cdd17c2c8
root <root@metanet.pri>
2023-05-10 12:01:28 +0900

    2023-05-10 12:01:28 content edit

--- autoUpload.sh
+++ autoUpload.sh
@@ -4,7 +4,7 @@ if [ $# -eq 1 ]; then
   cntxt=$1
   echo $cntxt
   sed -i '$ d' tuneindex.sh
-  echo "cat /usr/share/nginx/html/${cntxt} > /usr/share/nginx/html/index.html" >> tuneindex.sh
+  echo "cat /usr/share/nginx/html${cntxt} > /usr/share/nginx/html/index.html" >> tuneindex.sh
 fi
 
 msg=$(date +"%Y-%m-%d %T")" content edit"
--- tuneindex.sh
+++ tuneindex.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat /usr/share/nginx/html//day02/form.html > /usr/share/nginx/html/index.html
+cat /usr/share/nginx/html/day02/form.html > /usr/share/nginx/html/index.html
